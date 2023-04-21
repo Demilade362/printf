978404141b945c642dd58133aaa5b50cac40412b
Demilade362 <ademolademilade362@gmail.com>
2023-04-22 00:40:38 +0100

    Printf Again

---
+++ CONTRIBUTING.md
@@ -0,0 +1,10 @@
+### These are the names of the Contributors to this github repository
+
+* **Oluwademilade Abatan** [OLuwademilade's github](https://github.com/Demilade362)
+
+# To Contribute on This Project
+1. Fork the Repository.
+2. Clone it.
+3. Push it to your repository.
+4. Pull request(Merge it)
+5. One of the two admins will merge it if it is appropraite.
--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,8 @@
-#include "main.h"
-
+#include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - write character c to the stdout
+ * @c: the character
+ * Return: 1 on success and -1 otherwise
  */
 int _putchar(char c)
 {
